f9713f290b754aaf988b85eb13872f9744515ecf
Mich120201 <volpimichela3@gamil.com>
2023-05-18 11:41:27 +0200

    norm ok2

--- libft/lib/ft_count_arraystr.c
+++ libft/lib/ft_count_arraystr.c
@@ -12,13 +12,14 @@
 
 #include "../h_file/libft.h"
 
-int ft_count_arraystr(char **count)
+int	ft_count_arraystr(char **count)
 {
-	int i;
+	int	i;
+
 	i = -1;
-	if(!count)
+	if (!count)
 		return (0);
 	while (count[++i])
 		;
-	return(i);
+	return (i);
 }
